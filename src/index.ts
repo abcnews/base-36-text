8e3fb675455499d44529789299dc98e8a97dd5d4
Simon Elvery <simon@elvery.net>
2022-12-08 15:27:53 +1000

    Remove obsolte polyfills

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import 'fast-text-encoding';
 import baseX from 'base-x';
-import { Buffer } from 'buffer/';
 
 const BASE_36_CHARSET = '0123456789abcdefghijklmnopqrstuvwxyz';
-
 const base36 = baseX(BASE_36_CHARSET);
 
 export function encode(text: string): string {
-  return base36.encode(Buffer.from(new TextEncoder().encode(text)));
+  return base36.encode(new TextEncoder().encode(text));
 }
 
 export function decode(base36string: string): string {
